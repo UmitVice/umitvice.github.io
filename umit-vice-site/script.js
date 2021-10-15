d2b06bdd9d953df63531593d74212e0115856831
Ümithan Viçe <umitvice@gmail.com>
2021-10-15 11:16:45 +0300

    Site change
    Comlete Change

--- umit-vice-site/script.js
+++
@@ -1,6 +0,0 @@
-//navigation effect on scroll
-
-window.addEventListener("scroll", function(){
-  const header = document.querySelector("header");
-  header.classList.toggle('sticky', window.scrollY > 0)
-});
